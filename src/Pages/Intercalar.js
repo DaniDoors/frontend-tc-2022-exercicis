dc0866b85e09df3d3d96e1381cbf97bbd52d13ba
RadiaktiveLive <98389220+RadiaktiveLive@users.noreply.github.com>
2022-03-15 20:08:49 +0100

    tirorio

--- src/Pages/Intercalar.js
+++ src/Pages/Intercalar.js
@@ -1,3 +1,5 @@
+import ArrayBonic from "./ArrayBonic";
+
 export default function intercalar({ array1, array2 }) {
   let arrayConcatenat = [];
   for (let i = 0; i < array1.length || i < array2.length; i++) {
@@ -10,10 +12,11 @@ export default function intercalar({ array1, array2 }) {
       <h1> Array concatenat</h1>
       <div>
         arrays original 1:
+        <ArrayBonic array={array1} />
         <br />
-        array original 2:
+        array original 2: <ArrayBonic array={array2} />
         <br />
-        array concatenat: {arrayConcatenat}
+        array concatenat: <ArrayBonic array={arrayConcatenat} />
       </div>
     </div>
   );
